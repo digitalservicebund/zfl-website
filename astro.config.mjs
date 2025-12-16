3fd8fc421482669b7054d0d3dc8407e719960420
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-12-16 17:30:44 +0100

    Adapt text sizes, container

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
+import rehypeAddClasses from "rehype-add-classes";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-
+  markdown: {
+    rehypePlugins: [
+      [rehypeAddClasses, { a: "kern-link", p: "kern-body", li: "kern-body" }],
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
