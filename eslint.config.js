783e526b22f5cccbd25564d28c787b49c3fdd94f
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2026-01-06 16:00:44 +0100

    Add linting for Markdown

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 
 import eslint from "@eslint/js";
+import markdown from "@eslint/markdown";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import eslintPluginAstro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
@@ -10,5 +11,12 @@ export default defineConfig(
   tseslint.configs.recommended,
   eslintPluginAstro.configs.recommended,
   eslintConfigPrettier,
+  {
+    files: ["**/*.md"],
+    plugins: {
+      markdown,
+    },
+    extends: ["markdown/processor"],
+  },
   globalIgnores([".astro/"]),
 );
