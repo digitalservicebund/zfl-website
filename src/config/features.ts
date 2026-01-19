e7aaeced902103dc4a0974db3ff30a08bc746ed9
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2026-01-19 08:35:25 +0100

    Add staging indicator

--- src/config/features.ts
+++ src/config/features.ts
@@ -1,3 +1,8 @@
 export const FEATURES = {
   EXAMPLE: "example",
 } as const;
+
+const stage = import.meta.env.PUBLIC_STAGE || "production";
+
+export const isProduction = stage === "production";
+export const isDevelopment = stage === "development";
