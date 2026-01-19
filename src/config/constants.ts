83ee46fc840dbb5e460728362cd3a038a5ba7ae7
basti.paintner <basti.paintner@digitalservice.bund.de>
2026-01-19 16:02:19 +0100

    Add missing email to schulungen

---
+++ src/config/constants.ts
@@ -0,0 +1,9 @@
+export const ZFL_EMAIL = {
+  url: "mailto:support@zfl.bund.de",
+  display: "support@zfl.bund.de",
+};
+
+export const ZFL_PHONE = {
+  display: "0151/40 76 78 39",
+  url: "tel:015140767839",
+};
