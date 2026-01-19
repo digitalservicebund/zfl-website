f2bb51fe5589018beaf55ff8113eedb91a119464
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2026-01-19 10:04:56 +0100

    Remove log

--- src/config/features.ts
+++ src/config/features.ts
@@ -6,5 +6,3 @@ const stage = import.meta.env.PUBLIC_STAGE || "production";
 
 export const isProduction = stage === "production";
 export const isStaging = stage === "staging";
-
-console.log("stage", stage);
