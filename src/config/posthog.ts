1c0e3a1556271449c641fabdc5afdc88f59850b8
basti.paintner <basti.paintner@digitalservice.bund.de>
2026-01-22 11:49:43 +0100

    Change posthog key

--- src/config/posthog.ts
+++ src/config/posthog.ts
@@ -1,3 +1,3 @@
-export const posthogKey = "phc_72Ugpht0HD8AP1SvBcXoFXUT92tHPLrG89dChxfjKTN";
+export const posthogKey = "phc_BqKtG4bqKnrrhuUrplewJAgKOWxcm9ZE2W95HPYzbee"; // gitleaks:allow
 export const apiHost = "/ph-relay";
 export const uiHost = "https://eu.posthog.com";
