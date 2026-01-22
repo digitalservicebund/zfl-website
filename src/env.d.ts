ce8b0d1ea7b41e9e38d631f13ab439f423c62bd8
basti.paintner <basti.paintner@digitalservice.bund.de>
2026-01-22 09:57:25 +0100

    Fix pipeline

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly PUBLIC_POSTHOG_KEY: string;
-  readonly PUBLIC_POSTHOG_API_HOST: string;
-  readonly PUBLIC_POSTHOG_UI_HOST: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
