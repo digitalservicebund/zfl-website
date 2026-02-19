2d6b39510db5f9a132f39790ac50a58acacd41cb
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2026-02-19 16:24:32 +0100

    refactor: add showInHeader to hide elements on prod
    Move ueber uns pages

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,24 +1,29 @@
+import { isStaging } from "./stage";
+
 export type NavItem = {
   path: string;
   title: string;
+  showInHeader?: boolean;
   children?: NavItem[];
 };
 
 export const routes: Record<string, NavItem> = {
   begleitungen: { path: "/begleitungen", title: "Begleitungen" },
   schulungen: { path: "/schulungen", title: "Schulungen" },
-  // anleitungenUndHilfsmittel: {
-  //   path: "/anleitungen-und-hilfsmittel",
-  //   title: "Anleitungen und Hilfsmittel",
-  // },
-  // ueberUns: {
-  //   path: "/ueber-uns",
-  //   title: "Über uns",
-  //   children: [
-  //     { path: "/ueber-uns/zahlen-und-fakten", title: "Zahlen und Fakten" },
-  //     { path: "/ueber-uns/daran-arbeiten-wir", title: "Daran arbeiten wir" },
-  //   ],
-  // },
+  anleitungenUndHilfsmittel: {
+    path: "/anleitungen-und-hilfsmittel",
+    title: "Anleitungen und Hilfsmittel",
+    showInHeader: isStaging,
+  },
+  ueberUns: {
+    path: "/ueber-uns",
+    title: "Über uns",
+    showInHeader: isStaging,
+    children: [
+      { path: "/ueber-uns/zahlen-und-fakten", title: "Zahlen und Fakten" },
+      { path: "/ueber-uns/daran-arbeiten-wir", title: "Daran arbeiten wir" },
+    ],
+  },
 };
 
 export default Object.values(routes);
