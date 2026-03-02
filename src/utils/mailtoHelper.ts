7cdfac1d71e1bb6aab436826cbeff2c6fdc2edb5
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-03-02 17:30:31 +0100

    chore: fix sonarqube issues

--- src/utils/mailtoHelper.ts
+++ src/utils/mailtoHelper.ts
@@ -2,7 +2,7 @@ export function mailto(email: string, subject?: string, body?: string): string {
   const params = new URLSearchParams();
   if (subject) params.append("subject", subject);
   if (body) params.append("body", body);
-  const query = params.toString().replaceAll(/\+/g, "%20");
+  const query = params.toString().replaceAll("+", "%20");
   const queryStr = query ? `?${query}` : "";
   return `mailto:${email}${queryStr}`;
 }
