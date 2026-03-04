2625b281abf2241ac1d9a1a650a00438245c9ff0
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2026-03-04 12:13:21 +0100

    fix: csp headers for posthog

--- src/config/posthog.ts
+++ src/config/posthog.ts
@@ -1,3 +1,3 @@
 export const posthogKey = "phc_BqKtG4bqKnrrhuUrplewJAgKOWxcm9ZE2W95HPYzbee"; // gitleaks:allow
-export const apiHost = "https://eu.i.posthog.com";
+export const apiHost = "/ph-relay";
 export const uiHost = "https://eu.posthog.com";
