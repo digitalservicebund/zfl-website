3dd5a217c6df2f19d2416bd5794be7f0fd52a209
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-03-20 09:01:42 +0100

    test(e2e): exclude isStagingOnly pages
    404 redirects from middleware.ts trigger a11y reports in CI

--- e2e/a11y.spec.ts
+++ e2e/a11y.spec.ts
@@ -3,14 +3,18 @@ import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
 test.describe("a11y", () => {
-  Object.values(routes).forEach((route) => {
-    test(`${route.path} should not have any automatically detectable accessibility issues`, async ({
-      page,
-    }) => {
-      await page.goto(route.path);
+  Object.values(routes)
+    .filter((route) => !route.isStagingOnly)
+    .forEach((route) => {
+      test(`${route.path} should not have any automatically detectable accessibility issues`, async ({
+        page,
+      }) => {
+        await page.goto(route.path);
 
-      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-      expect(accessibilityScanResults.violations).toEqual([]);
+        const accessibilityScanResults = await new AxeBuilder({
+          page,
+        }).analyze();
+        expect(accessibilityScanResults.violations).toEqual([]);
+      });
     });
-  });
 });
