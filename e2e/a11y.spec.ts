7c3fe4f4522e4644b64383d674a55d35222191a4
daniel-marcus <47091062+daniel-marcus@users.noreply.github.com>
2026-03-20 09:52:45 +0100

    style: apply minimal kern styles (#99)
    * style: apply minimal kern styles
    
    * style: wrap header in .kern-container
    
    * style: add debug-grid
    
    * style: kern grid (wip)
    
    * fix(PageHeader): fix menu overlap
    
    * feat: new Über uns page
    
    * style: kernify pages
    
    * feat(PageHeader): dropdown for Kontakt
    
    * refactor(DebugGrid): move to separate component
    
    * ci(talisman): ignore images
    
    * test: fix a11y issues
    
    * ci(talisman): exluce false positives (images)
    
    * chore: semantic heading usage
    
    * style: adjust kern heading sizes
    
    * style: kern subline large -> default
    
    * style: rm padding around kern headings
    
    * style: kernify 404.astro
    
    * style: kernify footer
    
    * style(PageHeader): rm angie styles
    
    * style: replace angie w/ kern styles
    
    * perf: preload fonts
    
    * chore: disable DebugGrid in production
    
    * style: kern refinement & cleanup
    
    * test(e2e): exclude isStagingOnly pages
    
    404 redirects from middleware.ts trigger a11y reports in CI
    
    * test(e2e): honor isStaging
    
    * test(e2e): fix stage in nodejs env
    
    * fix(routesGenerator): typescript as direct dependency
    
    * chore(pages): fix order
    
    ---------
    
    Co-authored-by: Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>

--- e2e/a11y.spec.ts
+++ e2e/a11y.spec.ts
@@ -1,16 +1,21 @@
 import { routes } from "@/config/routes";
+import { isStaging } from "@/config/stage";
 import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
 test.describe("a11y", () => {
-  Object.values(routes).forEach((route) => {
-    test(`${route.path} should not have any automatically detectable accessibility issues`, async ({
-      page,
-    }) => {
-      await page.goto(route.path);
+  Object.values(routes)
+    .filter((route) => !route.isStagingOnly || isStaging)
+    .forEach((route) => {
+      test(`${route.path} should not have any automatically detectable accessibility issues`, async ({
+        page,
+      }) => {
+        await page.goto(route.path);
 
-      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-      expect(accessibilityScanResults.violations).toEqual([]);
+        const accessibilityScanResults = await new AxeBuilder({
+          page,
+        }).analyze();
+        expect(accessibilityScanResults.violations).toEqual([]);
+      });
     });
-  });
 });
