4ad589d955354c7e920217490851df991199ff21
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-04-22 09:33:58 +0200

    refactor: show isStagingOnly content in developtment
    ZFL-433

--- src/config/stage.ts
+++ src/config/stage.ts
@@ -1,6 +1,7 @@
 const stage =
-  import.meta.env?.PUBLIC_STAGE || process.env.PUBLIC_STAGE || "production";
+  import.meta.env?.PUBLIC_STAGE || process.env.PUBLIC_STAGE || "development";
 
 export const isProduction = stage === "production";
 export const isStaging = stage === "staging";
 export const isPreview = stage === "preview";
+export const isDevelopment = stage === "development";
