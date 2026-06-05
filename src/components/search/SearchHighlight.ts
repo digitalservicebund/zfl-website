3b211c44bb5e176d80fcc484f2dfaf09291aff61
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-06-05 18:30:24 +0200

    chore: fix sonarqube issue

--- src/components/search/SearchHighlight.ts
+++ src/components/search/SearchHighlight.ts
@@ -5,8 +5,8 @@ const highlightParam = "search";
 document.addEventListener("DOMContentLoaded", async () => {
   const base = import.meta.env.BASE_URL === "/" ? "" : import.meta.env.BASE_URL;
   await import(`${base}/pagefind/pagefind-highlight.js`);
-  new PagefindHighlight({
-    // NOSONAR
+  // prettier-ignore
+  new PagefindHighlight({ // NOSONAR
     highlightParam,
     markOptions: {
       className: "pagefind-highlight",
