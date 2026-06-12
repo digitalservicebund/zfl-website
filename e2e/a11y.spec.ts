c5ffc06f8133d090806410e88de1633beaf87dd4
Anne Ehrenpreis <anne.ehrenpreis@digitalservice.bund.de>
2026-06-12 13:53:56 +0200

    refactor(a11y): exclude dev routes from a11y test

--- e2e/a11y.spec.ts
+++ e2e/a11y.spec.ts
@@ -1,12 +1,26 @@
-import { allRoutes, staging } from "@/config/routes";
+import {
+  allRoutes,
+  dev,
+  dev_astroKomponenten,
+  dev_kernKomponenten,
+  staging,
+  vorhaben_steckbrief,
+} from "@/config/routes";
 import { isStaging } from "@/config/stage";
 import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
 test.describe("a11y", () => {
+  const excludedRouteKeys: string[] = [
+    staging.key,
+    dev.key,
+    dev_astroKomponenten.key,
+    dev_kernKomponenten.key,
+    vorhaben_steckbrief.key,
+  ];
   const relevantRoutes = allRoutes
     .filter((route) => !route.isStagingOnly || isStaging)
-    .filter((route) => route !== staging);
+    .filter((route) => !excludedRouteKeys.includes(route.key));
   relevantRoutes.forEach((route) => {
     test(`${route.path} should not have any automatically detectable accessibility issues`, async ({
       page,
