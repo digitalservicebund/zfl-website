a5bf4fb94dce69967157d30c8a6d9ae546995a5d
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-08-05 12:31:47 +0200

    refactor(Feature): unify --feature-spacing

--- src/pages/bessere-rechtsetzung/_Feature.svelte
+++ src/pages/bessere-rechtsetzung/_Feature.svelte
@@ -92,14 +92,14 @@
 </script>
 
 <div
-  class={`group grid grid-cols-[auto_1fr_auto] items-start gap-x-16 rounded-sm border border-(--border-color)`}
+  class={`group grid grid-cols-[auto_1fr_auto] items-start gap-x-(--feature-spacing) rounded-sm border border-(--border-color) [--feature-spacing:16px]`}
   title="Schritte hervorheben"
   style="--border-color: var(--kern-color-decorative-border-default);"
 >
   <button
     type="button"
     aria-expanded={details ? expanded : undefined}
-    class={`col-span-3 grid grid-cols-subgrid items-start max-md:gap-y-16 text-left focus-visible:outline-2 focus-visible:outline-(--border-color) p-16 hover:bg-(--border-color)/20 cursor-pointer ${expanded ? "bg-(--border-color)/20 _outline-2 outline-(--border-color)" : ""}`}
+    class={`col-span-3 grid grid-cols-subgrid items-start max-md:gap-y-(--feature-spacing) text-left focus-visible:outline-2 focus-visible:outline-(--border-color) p-(--feature-spacing) hover:bg-(--border-color)/20 cursor-pointer ${expanded ? "bg-(--border-color)/20" : ""}`}
     onclick={toggle}
     onmouseenter={previewOn}
     onmouseleave={previewOff}
@@ -135,7 +135,7 @@
       class={`md:col-start-2 max-md:col-span-3 max-md:col-start-1 grid transition-[grid-template-rows] duration-100 ease-in-out ${expanded ? "grid-rows-[1fr]" : "grid-rows-[0fr]"}`}
     >
       <div class="overflow-hidden">
-        <div class="py-16 max-md:px-16">
+        <div class="py-(--feature-spacing) max-md:px-(--feature-spacing)">
           {@render details?.()}
         </div>
       </div>
