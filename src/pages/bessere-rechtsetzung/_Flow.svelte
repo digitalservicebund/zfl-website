24a0d0e49e3f3e98fd9f34f0172c2c6c5570a20d
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-08-11 12:11:17 +0200

    refactor(Flow): rm unused tooltip texts

--- src/pages/bessere-rechtsetzung/_Flow.svelte
+++ src/pages/bessere-rechtsetzung/_Flow.svelte
@@ -38,15 +38,15 @@
       color="#E6E6E6"
       title="Initiative des Bundestages"
       className="max-sm:hidden"
-    ></Bubble>
+    />
     <Cluster color="#D59FDE" offset={0} sizes={["md"]}>
-      <Bubble title="Aus der Mitte der Regierung"></Bubble>
+      <Bubble title="Aus der Mitte der Regierung" />
     </Cluster>
     <Bubble
       color="#E6E6E6"
       title="Initiative des Bundesrates"
       className="max-sm:hidden"
-    ></Bubble>
+    />
   </div>
 
   <Arrow />
@@ -72,8 +72,8 @@
       sizes={["md", "md"]}
       ariaLabel="Schritte in dieser Phase: Arbeitsgruppenbildung, Workshops mit Ländern und Kommunen"
     >
-      <Bubble title="Arbeits&shy;gruppen&shy;bildung" optional></Bubble>
-      <Bubble title="Workshops mit Ländern und Kommunen" optional></Bubble>
+      <Bubble title="Arbeits&shy;gruppen&shy;bildung" optional />
+      <Bubble title="Workshops mit Ländern und Kommunen" optional />
     </Cluster>
 
     <Arrow>Übergabe an das Fachreferat</Arrow>
@@ -84,10 +84,7 @@
       sizes={["md"]}
       ariaLabel="Schritte in dieser Phase: Federführung"
     >
-      <Bubble title="Federführung">
-        Ein Fachreferat übernimmt die Verantwortung für die Weiterentwicklung
-        der Regelung. Legist:in wird zugewiesen.
-      </Bubble>
+      <Bubble title="Federführung" />
     </Cluster>
   </Section>
 
@@ -173,42 +170,23 @@
         optional
         tags={["Checks"]}
         size="lg"
-      ></Bubble>
+      />
       <Bubble
         title="Erarbeiten von Eckpunkten"
         optional
         size="md"
         tags={["Prozess"]}
-      >
-        Je nach Komplexität wird ein Eckpunktepapier verfasst oder nicht.
-      </Bubble>
-      <Bubble title="Vorschläge von Verbänden" optional size="sm"></Bubble>
-      <Bubble title="Gesetzes&shy;umfeld" size="sm" tags={["KI"]}>
-        <p>Wovon wird die Regelung beeinflusst?</p>
-        <ul>
-          <li>
-            Kontrolle aktiver und passiver Verweisungen innerhalb des
-            Gesetzgebungsverfahrends
-          </li>
-          <li>Kontrolle der verwendeten Rechtsbegriffe</li>
-          <li>Auffinden von kollidierenden Rechtsvorschriften</li>
-          <li>Feststellung der aktuellen Rechtslage</li>
-        </ul>
-      </Bubble>
-      <Bubble title="Gutachten und Sach&shy;verständigen&shy;kommissionen"
-      ></Bubble>
-      <Bubble title="Material&shy;recherche" size="sm" tags={["Checks"]}>
-        Recherchiert Daten, die relevant für das Gesetz sind sowie das
-        Gesetzesumfeld.
-      </Bubble>
+      />
+      <Bubble title="Vorschläge von Verbänden" optional size="sm" />
+      <Bubble title="Gesetzes&shy;umfeld" size="sm" tags={["KI"]} />
+      <Bubble title="Gutachten und Sach&shy;verständigen&shy;kommissionen" />
+      <Bubble title="Material&shy;recherche" size="sm" tags={["Checks"]} />
       <Bubble
         title="Ziel- und Wirkungs&shy;definition"
         size="md"
         tags={["Prozess", "Checks"]}
-      ></Bubble>
-      <Bubble title="Vorarbeit" size="sm" tags={["Prozess"]}>
-        Zieht Arbeit aus anderen Fach-Referaten über das Gesetzesthema zusammen.
-      </Bubble>
+      />
+      <Bubble title="Vorarbeit" size="sm" tags={["Prozess"]} />
     </Cluster>
   </Section>
 
@@ -229,29 +207,11 @@
       sizes={["sm", "sm", "sm", "sm", "sm"]}
       ariaLabel="Schritte in dieser Phase: Frühzeitige Beteiligung, Austausch, Vorblatt, Rohentwurf, Gesetzesfolgen besprechen"
     >
-      <Bubble title="Frühzeitige Beteiligung" optional size="sm">
-        Von Betroffenen und Vollzug und anderen Wissensträgern (z.B. Verbänden,
-        Ländern, Kommunen, Wissenschaft)
-      </Bubble>
-      <Bubble title="Austausch" size="sm">
-        Austausch mit Netzwerk, Expert:innen, anderen Ministerien, ggf.
-        Spiegelreferat
-      </Bubble>
-      <Bubble title="Vorblatt" size="sm">
-        <ul>
-          <li>Problem und Ziel</li>
-          <li>Lösung</li>
-          <li>Alternativen</li>
-          <li>Haushaltsausgaben ohne Erfüllungsaufwand</li>
-          <li>Erfüllungsaufwand</li>
-          <li>Weitere Kosten</li>
-        </ul>
-      </Bubble>
-      <Bubble title="Rohentwurf" size="sm">
-        Schreibt ersten Entwurf mit Kommentarspalte. Allein oder mit ein oder
-        mehreren weiteren Legist:innen zusammen.
-      </Bubble>
-      <Bubble title="Gesetzesfolgen besprechen" optional size="sm"></Bubble>
+      <Bubble title="Frühzeitige Beteiligung" optional size="sm" />
+      <Bubble title="Austausch" size="sm" />
+      <Bubble title="Vorblatt" size="sm" />
+      <Bubble title="Rohentwurf" size="sm" />
+      <Bubble title="Gesetzesfolgen besprechen" optional size="sm" />
     </Cluster>
   </Section>
 
@@ -278,16 +238,12 @@
       sizes={["sm", "sm", "lg"]}
       ariaLabel="Schritte in dieser Phase: Beteiligung der Referate, Hausentwurf erstellen, Frühkoordinierung/ Vorhabenclearing Bundeskanzleramt"
     >
-      <Bubble title="Beteiligung der Referate" size="sm">
-        Bittet hausinterne Referate um Feedback.
-      </Bubble>
-      <Bubble title="Hausentwurf erstellen" size="sm">
-        Ein mit den hausinternen Referaten abgeklärter Entwurf.
-      </Bubble>
+      <Bubble title="Beteiligung der Referate" size="sm" />
+      <Bubble title="Hausentwurf erstellen" size="sm" />
       <Bubble
         title="Frühkoordinierung/ Vorhabenclearing Bundeskanzleramt"
         size="lg"
-      ></Bubble>
+      />
     </Cluster>
 
     <Cluster
@@ -295,45 +251,11 @@
       sizes={["md", "sm", "md", "sm", "sm"]}
       ariaLabel="Schritte in dieser Phase: Gesetzesfolgenabschätzung, Beteiligung Dritter, Weitere Abstimmungen & Stellungnahmen, Ressortentwurf, Politische Abstimmung"
     >
-      <Bubble title="Gesetzes&shy;folgen&shy;abschätzung" size="md">
-        Beabsichtigte Wirkungen und unbeabsichtigte Nebenwirkungen
-        (Haushaltsausgaben, Demographische Auswirkungen, Nachhaltigkeitsaspekte,
-        Erfüllungsaufwand, Auswirkungen auf das Preisniveau)
-      </Bubble>
-      <Bubble title="Beteiligung Dritter" size="sm">
-        <p>
-          Einholung externer Expertise bspw. über Stellungnahmen, Anhörungen,
-          Fachgespräche, Workshops
-        </p>
-        <ul>
-          <li>Länder,</li>
-          <li>kommunale Spitzenverbänden,</li>
-          <li>Fachkreise,</li>
-          <li>Verbände</li>
-        </ul>
-      </Bubble>
-      <Bubble title="Weitere Abstimmungen & Stellung&shy;nahmen" size="md">
-        <p>
-          Es werden Stellungnahmen zum Hausentwurf eingeholt. Der Ressortentwurf
-          wird bei Bedarf angepasst.
-        </p>
-        <ul>
-          <li>Betroffene Ressorts</li>
-          <li>Bundesinnenministerium</li>
-          <li>Bundesjustizministerium</li>
-          <li>Normenkontrollrat</li>
-        </ul>
-      </Bubble>
-      <Bubble title="Ressort&shy;entwurf" size="sm">
-        Referententwurf wird angepasst und Regierungsentwurf vorbereitet
-      </Bubble>
-      <Bubble title="Politische Abstimmung" size="sm">
-        <p>Der Ressortentwurf wird bei Bedarf angepasst.</p>
-        <ul>
-          <li>Ressortabstimmung</li>
-          <li>Koalitionsausschuss</li>
-        </ul>
-      </Bubble>
+      <Bubble title="Gesetzes&shy;folgen&shy;abschätzung" size="md" />
+      <Bubble title="Beteiligung Dritter" size="sm" />
+      <Bubble title="Weitere Abstimmungen & Stellung&shy;nahmen" size="md" />
+      <Bubble title="Ressort&shy;entwurf" size="sm" />
+      <Bubble title="Politische Abstimmung" size="sm" />
     </Cluster>
 
     <Cluster
@@ -341,9 +263,7 @@
       sizes={["md"]}
       ariaLabel="Schritte in dieser Phase: Verbände informieren"
     >
-      <Bubble title="Verbände informieren" size="md">
-        Die betroffenen Verbände werden über den Ressortentwurf informiert.
-      </Bubble>
+      <Bubble title="Verbände informieren" size="md" />
     </Cluster>
   </Section>
 
@@ -365,16 +285,8 @@
       sizes={["md", "md"]}
       ariaLabel="Schritte in dieser Phase: Kabinettvorlage erstellen, Unterzeichnen der Kabinettvorlage"
     >
-      <Bubble title="Kabinett&shy;vorlage erstellen" size="md">
-        Die Kabinettvorlage beinhaltet neben dem Ressortentwurf weitere
-        Dokumente, die benötigt werden um das Gesetz im Kabinett zu behandeln
-        (Sprechzettel, Anschreiben, NKR Stellungnahme, Beteiligungen ...);
-        geregelt durch GGO* § 50.
-      </Bubble>
-      <Bubble title="Unterzeichnen der Kabinett&shy;vorlage" size="md">
-        Die Kabinettvorlage wird unterzeichnet von Unterabteilungsleitung,
-        Abteilungsleitung und Hausleitung (bzw. Staatssekretär:in in Vertretung)
-      </Bubble>
+      <Bubble title="Kabinett&shy;vorlage erstellen" size="md" />
+      <Bubble title="Unterzeichnen der Kabinett&shy;vorlage" size="md" />
     </Cluster>
 
     <Cluster
@@ -383,11 +295,7 @@
       sizes={["md"]}
       ariaLabel="Schritte in dieser Phase: Weiterleitung an das Bundeskanzleramt"
     >
-      <Bubble title="Weiterleitung an das Bundes&shy;kanzleramt" size="md">
-        Fachreferat leitet die Kabinettvorlage an Bundeskanzleramts-Chef:in
-        weiter (mind. 8 Tage vor der Kabinettsitzung). Einbindung Spiegel- &
-        Kabinettreferat im Rahmen des Zeichnungs-vorgangs.
-      </Bubble>
+      <Bubble title="Weiterleitung an das Bundes&shy;kanzleramt" size="md" />
     </Cluster>
   </Section>
 
@@ -412,21 +320,7 @@
       sizes={["md"]}
       ariaLabel="Schritte in dieser Phase: Stellungnahme Bundesrat"
     >
-      <Bubble title="Stellungnahme Bundesrat" color="#D2EDB9">
-        <h3>Weiterleitung an den Bundesrat</h3>
-        <p>
-          Chef:in des Bundeskanzleramts setzt die Kabinettvorlage auf die Agenda
-          der Kabinettsitzung. Leitet die Kabinettvorlage an den Bundesrat.
-        </p>
-        <h3>Stellungnahme</h3>
-        <p>
-          Der Bundesrat nimmt sich der Vorlage an und bezieht innerhalb von 6
-          Wochen Stellung dazu.
-        </p>
-        <h3>Weiterleitung an den Bundestag</h3>
-        Leitet Vorlage mit Stellungnahme und Gegenäußerung an den/die Präsident:in
-        des Bundestages.
-      </Bubble>
+      <Bubble title="Stellungnahme Bundesrat" color="#D2EDB9" />
     </Cluster>
 
     <Cluster
@@ -434,26 +328,7 @@
       sizes={["md"]}
       ariaLabel="Schritte in dieser Phase: Abstimmung im Bundestag"
     >
-      <Bubble title="Abstimmung im Bundestag">
-        <h3>Ausschuss</h3>
-        <p>
-          Regelung wird an den zuständigen Ausschuss geleitet, dort geprüft und
-          bearbeitet. Im Ausschuss wird ein Bericht verfasst mit einer
-          Beschlussempfehlung und dem Plenum vorgelegt.
-        </p>
-        <h3>Erste Lesung</h3>
-        <h3>Zweite Lesung</h3>
-        <p>
-          Der Gesetzesentwurf wird mit dem Ausschussbericht und den
-          Änderungsvorschlägen besprochen. Abstimmung und Einbringung von
-          Änderungsanträgen.
-        </p>
-        <h3>Dritte Lesung</h3>
-        <p>
-          Der Gesetzentwurf wird der <strong>Schlussabstimmung</strong> unterzogen.
-        </p>
-        <h3>Abstimmung im Bundesrat (optional)</h3>
-      </Bubble>
+      <Bubble title="Abstimmung im Bundestag" />
     </Cluster>
 
     <Cluster
@@ -461,20 +336,7 @@
       sizes={["md"]}
       ariaLabel="Schritte in dieser Phase: Verkündung"
     >
-      <Bubble title="Verkündung">
-        <h3>Verkündung</h3>
-        <p>
-          Der Gesetzestext wird im Bundesgesetzblatt veröffentlicht. Die
-          Kommentarspalte wird im Bundesanzeiger veröffentlicht.
-        </p>
-        <h3>Ausfertigung</h3>
-        <p>Bundespräsident:in prüft und unterzeichnet das Gesetz.</p>
-        <h3>Freigabe</h3>
-        <p>Freigegeben vom Ministerium zur Veröffentlichung</p>
-        <h3>Gegenzeichnung</h3>
-        <p>Bundeskanzler:in zeichnet das Gesetz gegen.</p>
-        <h3>Veröffentlichung Stellungnahme des NKR (optional)</h3>
-      </Bubble>
+      <Bubble title="Verkündung" />
     </Cluster>
   </Section>
 
@@ -500,27 +362,7 @@
       sizes={["md"]}
       ariaLabel="Schritte in dieser Phase: Dokumentation & Inkrafttreten"
     >
-      <Bubble title="Dokumentation & Inkrafttreten">
-        <h3>Übergabe der Regelung an die Dokumentations&shy;stelle</h3>
-        <h3>Dokumentation Gesetze</h3>
-        <p>
-          Metadaten werden hinzugefügt (z. B. Ministerium, amtliche Fundstelle,
-          basiert auf EU-Verordnung, ...)
-        </p>
-        <h3>Dokumentation Änderungsgesetze</h3>
-        <p>
-          Änderungen werden eingearbeitet. Metadaten werden hinzugefügt (z. B.
-          Ministerium, amtliche Fundstelle, basiert auf EU-Verordnung, ...)
-        </p>
-        <h3>Inkrafttreten</h3>
-        <p>Die Regelung tritt in Kraft und wird veröffentlicht.</p>
-        <ul>
-          <li>Veröffentlichung auf gesetze-im-internet.de</li>
-          <li>
-            Gesetz und Metadaten werden im Dokstellenportal veröffentlicht.
-          </li>
-        </ul>
-      </Bubble>
+      <Bubble title="Dokumentation & Inkrafttreten" />
     </Cluster>
 
     <Cluster
@@ -529,13 +371,7 @@
       sizes={["md"]}
       ariaLabel="Schritte in dieser Phase: Vollzug"
     >
-      <Bubble title="Vollzug">
-        <ul>
-          <li>Bildung von Arbeitsgruppen</li>
-          <li>Evaluation Änderungsdbedarfe</li>
-          <li>...</li>
-        </ul>
-      </Bubble>
+      <Bubble title="Vollzug" />
     </Cluster>
   </Section>
 
