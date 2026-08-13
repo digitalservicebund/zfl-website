60027a1e98af6336db56c71ef6a1d8583530955a
Daniel Marcus <daniel.marcus@digitalservice.bund.de>
2026-08-13 17:44:57 +0200

    test(testUtils): add svelte container renderer

--- src/utils/testUtils.ts
+++ src/utils/testUtils.ts
@@ -1,14 +1,17 @@
+import { getContainerRenderer } from "@astrojs/svelte/container-renderer";
 import {
   type ContainerRenderOptions,
   experimental_AstroContainer as AstroContainer,
 } from "astro/container";
+import { loadRenderers } from "astro:container";
 import { JSDOM } from "jsdom";
 
 export async function renderToDOM(
   component: import("astro/runtime/server/index.js").AstroComponentFactory,
   options?: ContainerRenderOptions,
 ) {
-  const container = await AstroContainer.create();
+  const renderers = await loadRenderers([getContainerRenderer()]);
+  const container = await AstroContainer.create({ renderers });
   const html = await container.renderToString(component, options);
   const window = new JSDOM(html).window;
   const dom = window.document;
