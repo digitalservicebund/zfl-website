a5d6ce5d616115f3c14d5503082917b799c2b7f5
daniel-marcus <daniel-marcus@users.noreply.github.com>
2026-09-21 13:59:05 +0000

    Deploying to gh-pages from @ digitalservicebund/zfl-website@11f93fb44d7a1edd14c2a0aa3853a938b1c5062a 🚀

---
+++ previews/mermaid/_astro/swimlanesDiagram-TC7HE7FX.iH5nL_sk.js
@@ -0,0 +1,8 @@
+import"./src.D83aMQU4.js";import{n as e}from"./chunk-Y2CYZVJY.DsF7k-Jl.js";import"./chunk-O7XYJQB3.DuE97Otl.js";import"./chunk-ZIGJFQKS.CieQB88A.js";import"./chunk-LNGE3PJU.Cz-PS4bJ.js";import"./chunk-7PRAP22T.CkRNrXQm.js";import"./chunk-742MDFTN.DMepnV9n.js";import"./chunk-MBY4JIJT.CVx4FQ6E.js";import"./chunk-7INBJB4K.D1egAbjq.js";import"./chunk-5DYCD2WN.Cbw53p6X.js";import"./chunk-UA2S7LBM.B9xAEpV8.js";import"./chunk-Z7XXMR3K.DUNG3Rwf.js";import"./chunk-GNY47TPC.CPg3dpUQ.js";import"./chunk-GWA4HPMP.DLuukaJM.js";import"./chunk-XXDRQBXY.CNSuccmt.js";import"./chunk-WEXAMYUT.BNLzQKF6.js";import{r as t,t as n}from"./chunk-7M6MHVWA.qk7PmC3_.js";var r=n({styles:e(e=>`${t(e)}
+  .swimlane.cluster:not([data-color-id]) rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
